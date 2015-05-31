0e4c762f6f8ed0683ded01301f9dc82659fac52c
YongHun Byun <yonghunbyun@gmail.com>
2015-05-31 19:26:47 +0900

    Update prompt settings

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,34 +1,41 @@
+# heavily inspired by the wonderful pure theme
+# https://github.com/sindresorhus/pure
+
+# For my own and others sanity
+# git:
+# %b => current branch
+# %a => current action (rebase/merge)
+# prompt:
+# %F => color dict
+# %f => reset color
+# %~ => current path
+# %* => time
+# %n => username
+# %m => shortname host
+# %(?..) => prompt conditional - %(condition.true.false)
+
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git # You can add hg too if needed: `git hg`
+zstyle ':vcs_info:git*' formats ' %b'
+zstyle ':vcs_info:git*' actionformats ' %b|%a'
+
 autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
 
 git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+    # check if we're in a git repo
+    command git rev-parse --is-inside-work-tree &>/dev/null || return
+
+    # check if it's dirty
+    command git diff --quiet --ignore-submodules HEAD &>/dev/null;
+    if [[ $? -eq 1 ]]; then
+        echo "%F{red}✗%f"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+        echo "%F{green}✔%f"
     fi
-  fi
 }
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+git_prompt_info() {
+ ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
 # echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
  echo "${ref#refs/heads/}"
 }
@@ -61,12 +68,21 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+# indicate a job (for example, vim) has been backgrounded
+suspended_jobs() {
+    sj=$(jobs 2>/dev/null | tail -n 1)
+    if [[ $sj == "" ]]; then
+        echo ""
+    else
+        echo "%{$FG[208]%}✱%{$reset_color%}"
+    fi
 }
 
 precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
+    vcs_info
+    print -P '\n%F{013}%~'
 }
+
+export PROMPT='%(?.%F{magenta}.%F{red})❯%f '
+export RPROMPT='`git_dirty`%F{241}$vcs_info_msg_0_%f `needs_push``suspended_jobs`'
+
