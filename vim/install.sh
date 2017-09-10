d5e12577f802e93625b23e350ff0167ff733a052
YongHun Byun <yonghunbyun@gmail.com>
2017-09-10 15:11:54 +0900

    Change vim bundle path

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # vim bundles directory
-vim_bundles="$HOME/vimfiles/bundle"
+# vim_bundles="$HOME/vimfiles/bundle"
 
 # make vim bundles dir
-if [ ! -d "$vim_bundles" ]; then
-    mkdir -p "$vim_bundles"
-fi
+# if [ ! -d "$vim_bundles" ]; then
+#    mkdir -p "$vim_bundles"
+# fi
