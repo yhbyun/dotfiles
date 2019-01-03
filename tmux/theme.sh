dcb376b99e09d4ce3d75bb084efb2470b895ed75
YongHun Byun <yonghunbyun@gmail.com>
2019-01-03 21:53:03 +0900

    Change tmux theme
    Excerpted from https://github.com/ahmedelgabri/dotfiles

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -1,61 +1,52 @@
-#### COLOUR
+# ----------------------
+# set some pretty colors
+# ----------------------
+# colorize messages in the command line
+set -g message-style bg=default,fg=brightred #base02
+set -g message-command-style bg=black,fg=blue
 
-tm_icon="♟"
-tm_color_active=colour213
-tm_color_inactive=colour241
-tm_color_feature=colour4
-tm_color_music=colour203
+# ----------------------
+# Panes
+# -----------------------
 
-# separators
-tm_separator_left_bold="◀"
-tm_separator_left_thin="❮"
-tm_separator_right_bold="▶"
-tm_separator_right_thin="❯"
+set -g pane-border-style bg=default,fg=colour244
+set -g pane-active-border-style bg=default,fg=colour202
+setw -g pane-border-status top
+setw -g pane-border-format  " \"#{pane_current_path}\" - \"#{pane_current_command}\" " 
 
-set -g status-left-length 32
-set -g status-right-length 150
+# ----------------------
+# Status Bar
+# -----------------------
+set -g status on
+
 set -g status-interval 5
+set -g status-justify left
+set -g status-position top
 
+# set color for status bar
+set -g status-style bg=default,fg=default
 
-# default statusbar colors
-# set-option -g status-bg colour0
-set-option -g status-fg $tm_color_active
-set-option -g status-bg default
-set-option -g status-attr default
+setw -g window-status-format "#[bg=default]#{?window_activity_flag,#{?window_bell_flag,#[fg=brightred]▲,#[fg=colour243]⦁},#[fg=colour238]⦁}#[bg=default]"
+setw -g window-status-current-format "#{?window_zoomed_flag,#[fg=yellow][ #W ],#W}#[bg=default]"
+# setw -g window-status-style bg=default,fg=colour237
+setw -g window-status-activity-style bg=default,fg=colour243
+setw -g window-status-bell-style bg=default,fg=brightred
+setw -g window-status-current-style bg=default,fg=colour004
 
-# default window title colors
-set-window-option -g window-status-fg $tm_color_inactive
-set-window-option -g window-status-bg default
-set -g window-status-format "#I #W"
+# show session name, window & pane number, date and time on left side of
+# status bar
+set -g status-left-length 70
+set -g status-left "#[fg=colour237]#{?client_prefix,#[fg=colour202],#[fg=colour245]}#S #[fg=colour237] "
 
-# active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
-set-window-option -g window-status-current-bg default
-set-window-option -g  window-status-current-format "#[bold]#I #W"
+# show default node version, battery status, wifi name & date time
+status_prefix="#{?client_prefix,#[fg=colour202]#(echo $(tmux show-option -gqv prefix | tr \"[:lower:]\" \"[:upper:]\" | sed 's/C-/\^/')) #[fg=colour237]⦁ ,}"
+status_php_version='#[fg=colour237]#[fg=#649C62]#(~/.dotfiles/tmux/scripts/tmux-php-version) #[fg=colour237]⦁ '
+status_node_version='#[fg=colour237]#[fg=#649C62]#(node -v) #[fg=colour237]⦁ '
+status_battery='#(battery -t -p -G) #[fg=colour237]⦁ '
+status_wifi="#[fg=colour237]#[fg=colour243]#(~/.dotfiles/tmux/scripts/tmux-wifi)"
+# for some reason that extra space at the end prevents the date from overlaping & makes it play nice with Nerd Fonts
+status_date_time="#[fg=colour237]#[fg=colour243]%A, %d %b %Y %H:%M#[fg=colour237] "
 
-# pane border
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_color_active
+set -g status-right-length 300
+set -g status-right "${status_prefix}${status_php_version}${status_node_version}${status_battery}${status_wifi}${status_date_time}"
 
-# message text
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
-
-# pane number display
-set-option -g display-panes-active-colour $tm_color_active
-set-option -g display-panes-colour $tm_color_inactive
-
-# clock
-set-window-option -g clock-mode-colour $tm_color_active
-
-tm_spotify="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/spotify.scpt)"
-tm_itunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/itunes.scpt)"
-tm_rdio="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/rdio.scpt)"
-tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
-
-tm_date="#[fg=$tm_color_inactive] %R %d %b"
-tm_host="#[fg=$tm_color_feature,bold]#h"
-tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
-
-set -g status-left $tm_session_name' '
-set -g status-right $tm_itunes' '$tm_rdio' '$tm_spotify' '$tm_date' '$tm_host
